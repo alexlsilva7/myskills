29e00b024ab03bda2df4c653995eb4ee0a9447a1
Alex Lopes <alex.lsilva154@gmail.com>
2022-10-15 21:49:24 -0300

    skillcard component

---
+++ src/components/SkillCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+export function SkillCard({skill}) {
+    return (
+        <TouchableOpacity  style={styles.buttonSkill}>
+            <Text style={styles.textSkill}>
+                {skill}
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonSkill: {
+        backgroundColor: '#1F1E25',
+        padding: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 10,
+        width: '100%',
+
+    },
+    textSkill: {
+        color: '#FFF',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+})
